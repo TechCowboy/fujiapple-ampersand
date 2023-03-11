f4196265aa18f051916e33ca1b5da47b44556043
Norman Davie <ecompservices@gmail.com>
2023-03-11 09:18:34 -0400

    updated basic programs and tried to fix automation for ciderpress

--- updatefile.py
+++ updatefile.py
@@ -4,10 +4,11 @@ import time
 from subprocess import Popen, PIPE
 import datetime
 
-filename   = [ "FUJIAPPLE"]
-pconvert   = [ 0x2000 ] # If entry is zero, it will get the address from pfilenames entry (dos 3 file)
-pfilenames = [ "FUJIAPPLE"  ]
-pfilenamed = [ "FUJIAPPLE" ]
+filename   = [ "FUJIAPPLE" ]
+pconvert   = [   0x2000    ] # If entry is zero, it will get the address from pfilenames entry (dos 3 file)
+pfilenames = [ "FUJIAPPLE" ] # SOURCE
+pfilenamed = [ "FUJIAPPLE" ] # DESTINATION
+
 dosdisk   = "FUJIAPPLE.dsk"
 prodisk   = "FUJIAPPLE.po"
 
@@ -15,13 +16,150 @@ global checked
 
 checked = False
 
-my_speed = 0.1
+window_speed = 0.4
+my_speed = 0.2
 tab_speed = 0.2
 
 debug = False
+debug_small = debug or False
 
+global ciderpress_window
+
+def run(cmdlist):
+    if debug:
+        for i in cmdlist:
+            print(i+" ", end='')
+        print()
+    
+    process = Popen(cmdlist, stdout=PIPE)
+    output, error = process.communicate()
+        
+    if output == None:
+        output = ""
+    else:
+        output = output.decode().strip()
+    if error == None:
+        error = ""
+    else:
+        error = error.decode().strip()
+    return [output,error]
+
+#**************************************************
+def set_focus(windowname):
+    if debug_small:
+        print("set_focus")
+
+    end_program = False
+    focus_set = False
+    while not focus_set:
+
+        error = ""
+        retry = 1
+        process_id1 = None
+        while retry > 0:
+            
+            #************** FIND THE WINDOW
+            if process_id1 == None:
+                # Run the console application with Popen
+                output, error = run(['xdotool', 'search', '--name', windowname])
+                    
+                if output == "":
+                    end_program = True
+                    print(error)
+                    exit(0)
+                    continue
+
+                # Print the output of the console application
+                process_id1 = output
+
+            #**************** set the window active
+            output, error = run(['xdotool', 'windowactivate', process_id1])
+                
+            if error != "":
+                process_id1 = None
+                print(error)
+                retry = 2
+                time.sleep(10)
+                end_program = True
+                
+            retry -= 1
+
+            time.sleep(1)
+            
+        
+        output, error = run(['xdotool', 'getwindowname', process_id1])
+        currentwindowname = output
+        
+        output, error = run(['wmctrl', '-a', currentwindowname])
+        
+        output, error = run(['xdotool', 'getactivewindow'])
+        process_id2 = output
+        
+        focus_set = process_id1 == process_id2
+ 
+        if focus_set:
+            print(f"focus given to {windowname} - {process_id1}")
+            print(f"{output}")
+            
+        else:
+            print("focus failed!")
+            exit(0)
+        
+        if end_program:
+            exit(0)
+
+
+def give_focus(windowname):
+    
+
+    if debug_small:
+        print(f"give_focus {windowname}")
+
+    output, error = run(['wmctrl', '-a', windowname])
+    
+    for i in range(2):
+        output, error = run(['xdotool', 'getactivewindow'])
+        process_id = output
+        if debug:
+            print(output)
+        
+        output, error = run(['xdotool', 'getwindowname', process_id])
+        currentwindowname = output
+        if debug:
+            print(f"current window: {currentwindowname}")
+    
+        if currentwindowname.find(windowname) != 0:
+            print(f"Not expected window '{currentwindowname}'")
+            output, err = run(['wmctrl', '-l'])
+            print(output)
+            print(error)
+            exit(0)
+    
+#**************************************************
+def start_ciderpress(disk):
+    global ciderpress_window
+
+    if debug_small:
+        print("start_ciderpress")
+
+    disk = "/home/ndavie/Documents/Projects/fujiapple-ampersand/" + disk
+    cmd_str = "wine /home/ndavie/Documents/Windows\ Programs/CiderPress/ciderpress.exe "+disk
+
+    proc = Popen([cmd_str], shell=True,
+             stdin=None, stdout=None, stderr=None, close_fds=True)
+    
+    time.sleep(1)
+    
+    ciderpress_window = "CiderPress"
+    give_focus(ciderpress_window)
+    
+    
+#**************************************************
 def erase_field():
     
+    if debug_small:
+        print("erase_field")
+
     p.keyDown("ctrl")
     p.press("a")
     p.keyUp("ctrl")
@@ -30,7 +168,11 @@ def erase_field():
     time.sleep(my_speed)
     
 
+#**************************************************
 def do_tabs(count):
+    if debug_small:
+        print("do_tabs")
+
     for i in range(count):
         if debug:
             print(f"{i+1}/{count} tab ", end='')
@@ -40,22 +182,31 @@ def do_tabs(count):
         print()
     time.sleep(my_speed)
     
+#**************************************************
 def find_file(file):
     global checked
+    
+    if debug_small:
+        print("find_file")
+
+    give_focus(ciderpress_window)
     
     if debug:
         print(f"find_file {file}")
     
     if debug:
         print("alt-e")
-    p.press("alt")
-    time.sleep(my_speed)
-    p.press("E")
+    p.keyDown("alt")
+    time.sleep(window_speed)
+    p.press("e")
+    p.keyUp("alt")
+    
     time.sleep(my_speed)
     if debug:
         print("F")
-    p.press("F")
+    p.press("f")
     time.sleep(my_speed)
+    give_focus("Find")
     
     p.keyDown("alt")
     p.press("n")
@@ -90,23 +241,36 @@ def find_file(file):
     do_tabs(1)
     p.press("enter")
         
-    time.sleep(0.5)
+    time.sleep(my_speed)
+    
+    give_focus(ciderpress_window)
 
+#**************************************************
 def add_file(files):
 
+    if debug_small:
+        print("add_file")
+
     for file in files:
-    
+        
+        time.sleep(my_speed)
+        
+        give_focus(ciderpress_window)
+        
         if debug:
             print(f"add_file {file}")
         
             print("alt-a")
-        p.press("alt")
+        p.keyDown("alt")
         p.press("A")
+        p.keyUp("alt")
         time.sleep(my_speed)
         if debug:
             print("F")
         p.press("F")
-        time.sleep(0.4)
+        time.sleep(1)
+        
+        give_focus("Add")
         
         p.keyDown("alt")
         p.press("d")
@@ -129,9 +293,62 @@ def add_file(files):
         
         p.press("enter")
         time.sleep(0.5)
+        
+    give_focus(ciderpress_window)
+        
+#**************************************************
+def del_file(files):
 
+    
+    if debug_small:
+        print("del_file")
 
+    for file in files:
+        
+        find_file(file)
+        
+        give_focus(ciderpress_window)
+        
+        if debug:
+            print(f"del_file {file}")
+            print("alt-a")
+        p.keyDown("alt")
+        p.press("A")
+        p.keyUp("alt")
+        time.sleep(my_speed)
+        if debug:
+            print("d")
+        p.press("d")
+        time.sleep(0.5)
+        
+        p.press("enter")
+        p.sleep(0.5)
+        
+        give_focus(ciderpress_window)
+
+#**************************************************
+def leave():
+    if debug_small:
+        print("leave")
+
+    give_focus(ciderpress_window)
+    
+    if debug:
+        print("alt-f")
+    p.keyDown("alt")
+    p.press("F")
+    p.keyUp("alt")
+    time.sleep(my_speed)
+    if debug:
+        print("E")
+    p.press("E")
+    time.sleep(my_speed)
+    
+#**************************************************
 def prodos_bin_file(files, address):
+
+    if debug_small:
+        print("prodos_bin_file")
 
     for file in files:
         find_file(file)
@@ -140,6 +357,7 @@ def prodos_bin_file(files, address):
             print(f"bin_file {file}")
         
             print("alt-a")
+        give_focus(ciderpress_window)
         p.press("alt")
         p.press("A")
         time.sleep(my_speed)
@@ -185,7 +403,11 @@ def prodos_bin_file(files, address):
         p.press("enter")
         time.sleep(0.5)
 
+#**************************************************
 def dos_bin_file(files):
+
+    if debug_small:
+        print("dos_bin_file")
 
     for file in files:
         find_file(file)
@@ -194,6 +416,7 @@ def dos_bin_file(files):
             print(f"bin_file {file}")
         
             print("alt-a")
+        give_focus(ciderpress_window)
         p.press("alt")
         p.press("A")
         time.sleep(my_speed)
@@ -233,47 +456,18 @@ def dos_bin_file(files):
         time.sleep(0.5)
         
 
-def del_file(files):
 
-    for file in files:
-        time.sleep(0.5)
 
-        find_file(file)
-        
-        if debug:
-            print(f"del_file {file}")
-            print("alt-a")
-        p.press("alt")
-        p.press("A")
-        time.sleep(my_speed)
-        if debug:
-            print("d")
-        p.press("d")
-        time.sleep(0.5)
-        
-        p.press("enter")
-        p.sleep(0.5)
-
-def leave():
-    if debug:
-        print("alt-f")
-    p.press("alt")
-    p.press("F")
-    time.sleep(my_speed)
-    if debug:
-        print("E")
-    p.press("E")
-    time.sleep(my_speed)
         
 
 def DOS3_version(disk, filename):
     #os.system("wine /home/ndavie/Documents/Windows\ Programs/CiderPress/ciderpress.exe /home/ndavie/Documents/Projects/fujiapple-ampersand/" + disk + " &")
 
-    cmd_str = "wine /home/ndavie/Documents/Windows\ Programs/CiderPress/ciderpress.exe /home/ndavie/Documents/Projects/fujiapple-ampersand/" + disk
+    if debug_small:
+        print("DOS3_version")
 
-    proc = Popen([cmd_str], shell=True,
-             stdin=None, stdout=None, stderr=None, close_fds=True)
-
+    start_ciderpress(disk)
+    
     print("click the title bar")
 
     t = 5
@@ -295,12 +489,12 @@ def DOS3_version(disk, filename):
 def ProDOS_version(disk, filename, address):
     #os.system("wine /home/ndavie/Documents/Windows\ Programs/CiderPress/ciderpress.exe /home/ndavie/Documents/Projects/fujiapple-ampersand/" + disk + " &")
 
-    cmd_str = "wine /home/ndavie/Documents/Windows\ Programs/CiderPress/ciderpress.exe /home/ndavie/Documents/Projects/fujiapple-ampersand/" + disk
+    if debug_small:
+        print("ProDOS_version")
 
-    proc = Popen([cmd_str], shell=True,
-             stdin=None, stdout=None, stderr=None, close_fds=True)
+    start_ciderpress(disk)
 
-    time.sleep(1)
+    
     print("**************************************")
     print("***** click the Ciderpress window ****")
     print("**************************************")
@@ -314,6 +508,7 @@ def ProDOS_version(disk, filename, address):
         t -= 1
     
     print()
+    
 
     del_file(filename)
 
@@ -327,6 +522,9 @@ def ProDOS_version(disk, filename, address):
 
 def convert_dos_bin_to_prodos(src, tgt):
     
+    if debug_small:
+        print("convert_dos_bin_to_prodos")
+
     file_in = open(src, 'rb')
     address = file_in.read(2)
     print(address)
@@ -348,6 +546,9 @@ def convert_dos_bin_to_prodos(src, tgt):
 
 def update_version(file, version_signature, new_version):
     
+    if debug_small:
+        print("update_version")
+
     updated = False
     
     fp = open(file, "rb")
@@ -373,10 +574,20 @@ def update_version(file, version_signature, new_version):
     
     return updated
     
+    
 if __name__ == "__main__":
+      
+    
+    # kill all running wine processes (ciderpress, applewin, etc.)
+    # to prevent locked files
+    cmd_str = "wineserver -k"
+
+    proc = Popen([cmd_str], shell=True,
+             stdin=None, stdout=None, stderr=None, close_fds=True)
+
+    time.sleep(2)
     
     do_dos = False
-    
     
     if do_dos:
         quit_program = False
@@ -395,6 +606,7 @@ if __name__ == "__main__":
     for i in range(len(pfilenames)):
         
         updated = update_version(pfilenames[i], version_signature, new_version)
+
         if not updated:
             new_version = "Version not updated"
         else:    
